12e1c57c239ea3d5e9f53247b0e4a2bfa2a43e5d
Gambhiro <gambhiro.bhikkhu.85@gmail.com>
2016-05-16 16:56:22 +0100

    test new data

--- data/get_days.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-curl --data "calendar=mahanikaya&fromYear=2014&toYear=2018" -o days.json "http://localhost:8000/days.json"
--- ical/get_mahanikaya.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-curl --data "calendar=mahanikaya&fromYear=2013&toYear=2017" -o mahanikaya.ical "http://localhost:8000/days.ical"
